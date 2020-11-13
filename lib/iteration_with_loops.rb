b807235285819fc6e95b7a99bcb11358083f451b
Jen Tracy <jenniferetracy@gmail.com>
2020-11-13 22:34:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ def find_even_values(src)
    while element_count<src[row_count].count do
      if src[row_count][element_count].even?
        p src[row_count][element_count]
-     
+     end
      element_count += 1
  end
  row_count += 1
